da9eeef0c628c7a63ced673ea812540bb98af14d
Furkan Melih Ercan <furkan.ercan@b2metric.com>
2025-11-02 23:17:07 +0300

    feat: telemetry collector middleware & test micro services & docker-compose setup

--- order.py
+++ order.py
@@ -13,13 +13,8 @@ app.add_middleware(UnifiedLoggingMiddleware)
 http_client = httpx.AsyncClient()
 
 
-async def call_shipping_service(order_id: str, trace_id: str, user_id: str):
-    trace_id_var.set(trace_id)
-    log.info(f"Dropping message on shipping-queue: Order {order_id} will be shipped.")
-
-
 @app.post("/order")
-async def create_order(request: Request, background_tasks: BackgroundTasks):
+async def create_order(request: Request):
     data = await request.json()
     user_id = data.get("user_id", "unknown_user")
     item_count = data.get("item_count", 0)
@@ -45,8 +40,6 @@ async def create_order(request: Request, background_tasks: BackgroundTasks):
             exc_info=True,
         )
         raise HTTPException(status_code=400, detail="Payment failed")
-
-    background_tasks.add_task(call_shipping_service, order_id, trace_id, user_id)
 
     log.info(f"Order {order_id} confirmed successfully.")
     return {"status": "Order received", "order_id": order_id}
